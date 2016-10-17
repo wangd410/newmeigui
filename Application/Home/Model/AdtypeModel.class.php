e30f0b12b0b23eacc476bbd86e6e79a88b85d6e9
wangd410 <546520282@qq.com>
2016-10-17 15:28:05 +0800

    添加方法

--- Application/Home/Model/AdtypeModel.class.php
+++ Application/Home/Model/AdtypeModel.class.php
@@ -5,5 +5,9 @@ use Think\Model;
 
 class AdtypeModel extends Model
 {
-
+      public  function get_adType () {
+            $adType = M('Adtype');
+            $data = $adType->select();
+            return $data;
+      }
 }
